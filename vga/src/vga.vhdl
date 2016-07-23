33a762bb2e6bc8c7a1cc10abbe243f9fc3b3585b
Stuart Wallace <stuartw@atom.net>
2016-07-23 14:10:22 +0100

    Added pipelining to VGA scan-out, first attempt

--- vga/src/vga.vhdl
+++ vga/src/vga.vhdl
@@ -5,17 +5,17 @@
 
 -- VGA timings
 --
--- ----------------  -------- horizontal -------  -------- vertical ---------  -------  --- pll ---
--- resolution  rfsh  sync   fp    bp    pix  pol  sync   fp    bp    pix  pol  clk/MHz  divf   divq
--- ----------------  ---------------------------  ---------------------------  -------  -----------
---  640 x  480  @60    96   16    48    640    0     2   10    33    480    0   25.175  1000010 101
+-- ----------------  -------- horizontal -------  -------- vertical ---------  -------  --- pll ---  -- pllx8 --
+-- resolution  rfsh  sync   fp    bp    pix  pol  sync   fp    bp    pix  pol  clk/MHz  divf   divq  divf   divq
+-- ----------------  ---------------------------  ---------------------------  -------  -----------  -----------
+--  640 x  480  @60    96   16    48    640    0     2   10    33    480    0   25.175  1000010 101  1000010 010
 --  800 x  600  @60   128   40    88    800    1     4    1    23    600    1   40.000  0110100 100
 -- 1024 x  768  @60   136   24   160   1024    0     6    3    29    768    0   65.000  1010110 100
 -- 1280 x 1024  @60   112   48   248   1280    1     3    1    38   1024    1  108.000  1000111 011
 -- 1440 x  900  @60   152   80   232   1440    0     3    1    28    900    1  106.470  1000110 011
 -- 1680 x 1050  @60   184  104   288   1680    0     3    1    33   1050    1  147.140  0110000 010
 -- 1920 x 1080  @60   207  119   326   1920    0     3    1    32   1080    0  172.221  0111000 010
--- ----------------  ---------------------------  ---------------------------  ------- ------------
+-- ----------------  ---------------------------  ---------------------------  -------  -----------  -----------
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -27,8 +27,8 @@ entity vga_gen is
     port(
         CLK         : in std_logic;         -- master clock input (12MHz)
 
-        HSYNC       : out std_logic;        -- horizontal sync
-        VSYNC       : out std_logic;        -- vertical sync
+        H_SYNC      : out std_logic;        -- horizontal sync
+        V_SYNC      : out std_logic;        -- vertical sync
 
         R           : out std_logic;        -- red output
         G           : out std_logic;        -- green output
@@ -38,72 +38,97 @@ end;
 
 
 architecture behaviour of vga_gen is
+    signal core_clk     : std_logic;
+    signal pix_clk      : std_logic;
+
     signal disp_en      : std_logic;
 
     signal x            : std_logic_vector(11 downto 0);
     signal y            : std_logic_vector(11 downto 0);
+
+    signal pix_val      : std_logic_vector(2 downto 0);
 begin
 
-    -- instantiate a VGA controller module
-    vga_controller_inst: entity work.vga_controller 
+    -- instantiate a clock-generator PLL, fixed at 201.4MHz, =8 x 25.175MHz (for 640x480)
+    pll_inst: entity work.pll 
         generic map(
-            h_sync_width    => 207,
-            h_fp_width      => 119,
-            h_bp_width      => 326,
-            h_pixels        => 1920,
-            h_sync_pol      => '0',
-
-            v_sync_width    => 3,
-            v_fp_width      => 1,
-            v_bp_width      => 32,
-            v_pixels        => 1080,
-            v_sync_pol      => '0',
-
-            pll_divf        => "0111000",
+            pll_divf        => "1000010",
             pll_divq        => "010"
         )
         port map(
-            ref_clk         => CLK,
-            reset           => '1',
-
-            h_sync          => HSYNC,
-            v_sync          => VSYNC,
-
-            disp_en         => disp_en,
-
-            col             => x,
-            row             => y
+            REFERENCECLK    => CLK,
+            PLLOUTCORE      => core_clk,
+            PLLOUTGLOBAL    => open,
+            RESET           => '1'          -- TODO find out whether the chip has a reset input wired
         );
 
-    process(disp_en, x, y)
+    -- produce the pixel clock = clk/8
+    process(core_clk)
+        variable div : integer range 0 to 3 := 0;
     begin
-        if(disp_en = '1') then
-            -- inside the active display area: light all the red and blue pixels
-            -- (could use the values of the "x" and "y" signals to determine what to display here)
-            if(x(5) = '0' and x(6) = '0') then
-                R <= '0';
-                G <= '0';
-            elsif(x(5) = '1' and x(6) = '0') then
-                R <= '1';
-                G <= '0';
-            elsif(x(5) = '0' and x(6) = '1') then
-                R <= '0';
-                G <= '1';
-            elsif(x(5) = '1' and x(6) = '1') then
-                R <= '1';
-                G <= '1';
+        if(rising_edge(core_clk)) then
+            div := div + 1;
+            if(div = 0) then
+                pix_clk <= not pix_clk;
+            end if;
+        end if;
+    end process;
+
+    -- instantiate a VGA controller module; set resolution to 640x480 @60Hz
+    vga_controller_inst: entity work.vga_controller 
+        generic map(
+            vga_h_sync      => 96,
+            vga_h_fp        => 16,
+            vga_h_bp        => 48,
+            vga_h_pixels    => 640,
+            vga_h_sync_pol  => '0',
+
+            vga_v_sync      => 2,
+            vga_v_fp        => 10,
+            vga_v_bp        => 33,
+            vga_v_pixels    => 480,
+            vga_v_sync_pol  => '0'
+        )
+        port map(
+            CLK             => pix_clk,
+            RESET           => '1',
+
+            PIXEL           => pix_val,
+            H_SYNC          => H_SYNC,
+            V_SYNC          => V_SYNC,
+
+            DISP_EN         => disp_en,
+
+            COL             => x,
+            ROW             => y,
+
+            R               => R,
+            G               => G,
+            B               => B
+        );
+
+    process(x, y, pix_clk)
+        variable pix_data : std_logic_vector(2 downto 0);
+    begin
+        if(rising_edge(pix_clk)) then
+            if(x(6) = '0') then
+                pix_data(0) := '0';
+            else
+                pix_data(0) := '1';
             end if;
 
-            if(y(5) = '0') then
-                B <= '0';
+            if(x(7) = '0') then
+                pix_data(1) := '0';
             else
-                B <= '1';
+                pix_data(1) := '1';
             end if;
-        else
-            -- outside the active display area
-            R <= '0';
-            G <= '0';
-            B <= '0';
+
+            if(y(6) = '0') then
+                pix_data(2) := '0';
+            else
+                pix_data(2) := '1';
+            end if;
+            pix_val <= pix_data;
         end if;
     end process;
 end behaviour;
--- vga/src/vga_controller.vhdl
+++ vga/src/vga_controller.vhdl
@@ -11,60 +11,53 @@ use ieee.numeric_std.all;
 entity vga_controller is
     generic(
         -- Horizontal
-        h_sync_width    : integer;      -- sync pulse width, pixels
-        h_fp_width      : integer;      -- "front porch" width, pixels
-        h_bp_width      : integer;      -- "back porch" width, pixels
-        h_pixels        : integer;      -- visible pixel count
-        h_sync_pol      : std_logic;    -- sync polarity (1: +ve, 0: -ve)
+        vga_h_sync      : integer;      -- sync pulse width, pixels
+        vga_h_fp        : integer;      -- "front porch" width, pixels
+        vga_h_bp        : integer;      -- "back porch" width, pixels
+        vga_h_pixels    : integer;      -- visible pixel count
+        vga_h_sync_pol  : std_logic;    -- sync polarity (1: +ve, 0: -ve)
 
         -- Vertical
-        v_sync_width    : integer;      -- sync pulse width, rows
-        v_fp_width      : integer;      -- "front porch" width, rows
-        v_bp_width      : integer;      -- "back porch" width, rows
-        v_pixels        : integer;      -- visible pixel count
-        v_sync_pol      : std_logic;    -- sync polarity (1: +ve, 0: -ve)
-
-        -- PLL attributes
-        pll_divf        : bit_vector(6 downto 0);
-        pll_divq        : bit_vector(2 downto 0)
+        vga_v_sync      : integer;      -- sync pulse width, rows
+        vga_v_fp        : integer;      -- "front porch" width, rows
+        vga_v_bp        : integer;      -- "back porch" width, rows
+        vga_v_pixels    : integer;      -- visible pixel count
+        vga_v_sync_pol  : std_logic     -- sync polarity (1: +ve, 0: -ve)
     );
 
     port(
-        REF_CLK         : in  std_logic;    -- reference clock
+        CLK             : in  std_logic;    -- reference clock
         RESET           : in  std_logic;    -- active-low async reset
 
         H_SYNC          : out std_logic;    -- horizontal sync
         V_SYNC          : out std_logic;    -- vertical sync
 
+        R               : out std_logic;    -- red output
+        G               : out std_logic;    -- green output
+        B               : out std_logic;    -- blue output
+
         DISP_EN         : out std_logic;    -- display enable
 
-        COL             : out std_logic_vector(11 downto 0);      -- h co-ordinate
-        ROW             : out std_logic_vector(11 downto 0)       -- v co-ordinate
+        COL             : out std_logic_vector(11 downto 0);    -- h co-ordinate of next pixel
+        ROW             : out std_logic_vector(11 downto 0);    -- v co-ordinate of next pixel
+
+        PIXEL           : in  std_logic_vector(2 downto 0)      -- pixel data
     );
 end vga_controller;
 
 
 architecture behaviour of vga_controller is
-    constant h_period   : integer := h_sync_width + h_bp_width + h_pixels + h_fp_width;
-    constant v_period   : integer := v_sync_width + v_bp_width + v_pixels + v_fp_width;
+    constant h_period   : integer := vga_h_sync + vga_h_bp + vga_h_pixels + vga_h_fp;
+    constant v_period   : integer := vga_v_sync + vga_v_bp + vga_v_pixels + vga_v_fp;
 
-    signal clk : std_logic;
+    signal pix_val_p2   : std_logic_vector(2 downto 0);
+    signal pix_val_p3   : std_logic_vector(2 downto 0);
+    signal h_sync_p1    : std_logic;
+    signal h_sync_p2    : std_logic;
+    signal v_sync_p1    : std_logic;
+    signal v_sync_p2    : std_logic;
 begin
-
-    -- instantiate a clock-generator PLL, fixed at 25.175MHz (for 640x480)
-    vga_pll_inst: entity work.vga_pll 
-        generic map(
-            divf            => pll_divf,
-            divq            => pll_divq
-        )
-        port map(
-            REFERENCECLK    => REF_CLK,
-            PLLOUTCORE      => clk,
-            PLLOUTGLOBAL    => open,
-            RESET           => RESET
-        );
-
-    process(clk, RESET)
+    process(CLK, RESET)
         variable h_count : integer range 0 to h_period - 1 := 0;
         variable v_count : integer range 0 to v_period - 1 := 0;
     begin
@@ -72,13 +65,56 @@ begin
             h_count := 0;
             v_count := 0;
 
-            H_SYNC  <= not h_sync_pol;
-            V_SYNC  <= not v_sync_pol;
+            H_SYNC  <= not vga_h_sync_pol;
+            V_SYNC  <= not vga_v_sync_pol;
             DISP_EN <= '0';
 
             COL <= (others => '0');
             ROW <= (others => '0');
-        elsif(rising_edge(clk)) then
+        elsif(rising_edge(CLK)) then
+            --
+            -- Pipeline stage 3: emit pixel and sync signals
+            --
+            pix_val_p3  <= pix_val_p2;
+            H_SYNC      <= h_sync_p2;
+            V_SYNC      <= v_sync_p2;
+
+            if((h_count < vga_h_pixels) and (v_count < vga_v_pixels)) then
+                if(pix_val_p3(2) = '1') then
+                    R <= '1';
+                else
+                    R <= '0';
+                end if;
+
+                if(pix_val_p3(1) = '1') then
+                    G <= '1';
+                else
+                    G <= '0';
+                end if;
+
+                if(pix_val_p3(0) = '1') then
+                    B <= '1';
+                else
+                    B <= '0';
+                end if;
+            else
+                R <= '0';
+                G <= '0';
+                B <= '0';
+            end if;
+
+
+            --
+            -- Pipeline stage 2: read pixel value and latch; move sync signals along the pipeline.
+            --
+            pix_val_p2 <= PIXEL;
+            h_sync_p2 <= h_sync_p1;
+            v_sync_p2 <= v_sync_p1;
+
+            --
+            -- Pipeline stage 1: calculate next pixel address and latch it on ROW and COL.
+            -- Calculate the hsync & vsync values and latch them.
+            --
             if(h_count < (h_period - 1)) then
                 h_count := h_count + 1;
             else
@@ -91,30 +127,35 @@ begin
             end if;
 
             -- generate horizontal sync
-            if((h_count < (h_pixels + h_fp_width)) or (h_count > (h_pixels + h_fp_width + h_sync_width))) then
-                H_SYNC <= not h_sync_pol;
+            if((h_count >= (vga_h_pixels + vga_h_fp)) and
+               (h_count <= (vga_h_pixels + vga_h_fp + vga_h_sync))) then
+                h_sync_p1 <= vga_h_sync_pol;
             else
-                H_SYNC <= h_sync_pol;
+                h_sync_p1 <= not vga_h_sync_pol;
             end if;
-    
+            
             -- generate vertical sync
-            if((v_count < (v_pixels + v_fp_width)) or (v_count > (v_pixels + v_fp_width + v_sync_width))) then
-                V_SYNC <= not v_sync_pol;
+            if((v_count >= (vga_v_pixels + vga_v_fp)) and
+               (v_count <= (vga_v_pixels + vga_v_fp + vga_v_sync))) then
+                v_sync_p1 <= vga_v_sync_pol;
             else
-                V_SYNC <= v_sync_pol;
+                v_sync_p1 <= not vga_v_sync_pol;
             end if;
-    
+
             -- generate pixel co-ordinates
-            if(h_count < h_pixels) then
+            if(h_count < vga_h_pixels) then
                 COL <= std_logic_vector(to_unsigned(h_count, COL'length));
+            else
+                COL <= (others => '0');
             end if;
     
-            if(v_count < v_pixels) then
+            if(v_count < vga_v_pixels) then
                 ROW <= std_logic_vector(to_unsigned(v_count, ROW'length));
+            else
+                ROW <= (others => '0');
             end if;
-    
-            -- generate display enable
-            if((h_count < h_pixels) and (v_count < v_pixels)) then
+
+            if((h_count < vga_h_pixels) and (v_count < vga_v_pixels)) then
                 DISP_EN <= '1';
             else
                 DISP_EN <= '0';
